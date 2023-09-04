d5189e9b03249b889eb6bf8bf256dd47ce8084c0
Rostyslav Mysiuha <rossmysiuga@gmail.com>
2023-09-04 14:36:46 -0400

    Added qrcode functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import QRCode from 'react-qr-code';
 
 function App() {
+  const [inputValue, setInputValue] = React.useState('');
   return (
-    <div>
-      Test
+    <div className="container mx-auto w-2/4">
+      <h1 className="py-4">Code contents</h1>
+      <input
+      onChange={e => setInputValue(e.target.value)} 
+      type="text"
+      placeholder="Enter a link for QR code"
+      className="border border-gray-300 w-full h-8 p-4 font-light text-sm focus:outline-none focus:border-gray-600"></input>
+
+      <QRCode 
+      className="mx-auto mt-20"
+      value={inputValue} />
     </div>
   );
 }
